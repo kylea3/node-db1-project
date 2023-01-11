d72c8b9a38f64019dc381a2bacd82486e1eeb7ee
Kyle Andrus <kyleandrus3@gmail.com>
2023-01-11 10:31:22 -0700

    create get response for all accounts and specific account ids in account-router.js

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,13 +1,23 @@
 const router = require('express').Router()
-const middleware = require('./accounts-middleware')
+const {checkAccountId} = require('./accounts-middleware')
 const Account = require('./accounts-model')
 
 router.get('/', (req, res, next) => {
   // DO YOUR MAGIC
+  Account.getAll()
+    .then(account => {
+      res.status(200).json(account)
+    })
+    .catch(next)
 })
 
-router.get('/:id', (req, res, next) => {
+router.get('/:id', checkAccountId, (req, res, next) => {
   // DO YOUR MAGIC
+  Account.getById(req.params.id)
+    .then(account => {
+      res.status(200).json(account)
+    })
+    .catch(next)
 })
 
 router.post('/', (req, res, next) => {
--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const server = express();
 
-const accountRouter = require('./accounts/accounts-router')
+const accountsRouter = require('./accounts/accounts-router')
 
 server.use(express.json());
 
