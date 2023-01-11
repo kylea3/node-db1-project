f06a7a00f96823e1b35e78e95a4a9e0bbc31c1b0
Kyle Andrus <kyleandrus3@gmail.com>
2023-01-10 17:35:56 -0700

    insert SQL queries in queries.sql and knex queries in accounts-model.js

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,21 +1,34 @@
 const getAll = () => {
   // DO YOUR MAGIC
+  db('accounts')
 }
 
 const getById = id => {
   // DO YOUR MAGIC
+  db('accounts')
+  .where('id', id)
+  .first()
 }
 
 const create = account => {
   // DO YOUR MAGIC
+  db('accounts')
+  .insert(account)
+
 }
 
 const updateById = (id, account) => {
   // DO YOUR MAGIC
+  db('accounts')
+  .where('id', id)
+  .update(account)
 }
 
 const deleteById = id => {
   // DO YOUR MAGIC
+  db('accounts')
+  .where('id', id)
+  .delete()
 }
 
 module.exports = {
